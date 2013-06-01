a15fc158e5c98324b5cf3eaf4c3ff0953976bb65
Florin Popa <flopaconsult@gmail.com>
2013-06-02 00:36:07 +0300

    changes from florin-wrk on Sun Jun 2 00:36:07 EEST 2013

--- files/default/tests/minitest/webonly_test.rb
+++ files/default/tests/minitest/webonly_test.rb
@@ -7,4 +7,23 @@ include Helpers::Magento
     directory("/etc/chef").must_exist
   end
   
+    it 'listens on port 80' do
+    apache_configured_ports.must_include(80)
+  end
+
+%w{
+headers
+rewrite
+}.each do |expected_module|
+
+  describe "apache2::mod_#{expected_module}" do
+    include Helpers::Apache
+
+    it "installs mod_#{expected_module}" do
+      apache_enabled_modules.must_include "#{expected_module}_module"
+    end
+
+  end
+end  
+  
 end
